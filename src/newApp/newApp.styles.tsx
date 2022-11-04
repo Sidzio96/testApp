6cea9eb85adc3020ce883f77d909a3d31445b2dd
Sidzio96 <kubol96@onet.pl>
2022-11-04 11:00:35 +0100

    rename project and add fetch api

--- src/newApp/newApp.styles.tsx
+++ src/newApp/newApp.styles.tsx
@@ -10,15 +10,6 @@ export const AppWrapper = styled.div`
   justify-content: center;
   color: blue;
 `;
-export const ButtonsWrapper = styled.div`
-  display: flex;
-  padding-top: 20px;
-`;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  padding-right: 24px;
-`;
 
 export const LabelWrapper = styled.div`
   margin: 0px 0px 8px 0px;
@@ -31,4 +22,9 @@ export const LabelWrapper = styled.div`
 `;
 export const DescWrapper = styled.div`
   margin: 8px 0px 0px 0px;
+  font-weight: 500;
+  font-size: 18px;
+  display: block;
+  cursor: pointer;
+  color: white;
 `;
--- src/newApp/newApp.tsx
+++ src/newApp/newApp.tsx
@@ -1,11 +1,50 @@
 import React from "react";
 import AntdAutoComplete from "antd/lib/auto-complete";
-import Button from "antd/lib/button";
+import Input from "antd/lib/input"
 import 'antd/dist/antd.css';
 import * as S from './newApp.styles';
 
 
 const NewApp:React.FC = () => {
+    const [value, setValue] = React.useState<string>('');
+    const [results, setResults] = React.useState<string[]>([]);
+    React.useEffect(() => {
+        const api = async () => {
+            const data = await fetch(`https://jsonplaceholder.typicode.com/todos?q=${value}`);
+            const jsonData = await data.json();
+            setResults(jsonData);
+        };
+        api();
+     }, [value]);
+
+    const handleSearch = (value: string) => {
+        let result: any
+        if (!value || value.indexOf('@') >= 0) {
+            result = [];
+        } else {
+            result = results.filter(item => Object.values(item)
+                .join("")
+                .toLowerCase()
+                .includes(value.toLowerCase()));
+        }
+        setResults(result);
+    };
+
+    const extractContent = (str: string) => {
+        const span = document.createElement('span');
+        span.innerHTML = str;
+        return span.textContent || span.innerText;
+    };
+    const renderItem2 = results.map((result: any) => {
+        return (
+            <div key={result.toString()}>
+                <span style={{ fontWeight: 400 }}>{JSON.stringify(result.title)}</span>
+            </div>
+        );
+    })
+
+    const options = [{label: renderItem2}];
+
     return (
         <div>
             <S.AppWrapper>
@@ -13,15 +52,21 @@ const NewApp:React.FC = () => {
                     <S.LabelWrapper>
                         Label
                     </S.LabelWrapper>
-                  <AntdAutoComplete
-                      style={{width: 200}}/>
+                    <AntdAutoComplete
+                        onSearch={handleSearch}
+                        popupClassName="certain-category-search-dropdown"
+                        dropdownMatchSelectWidth={500}
+                        onChange={(value: string) => {
+                            setValue(extractContent(value));
+                            handleSearch(extractContent(value));
+                        }}
+                        style={{ width: 250 }}
+                        value={value === 'undefined' ? '' : value}
+                        options={options}
+                    >
+                        <Input.Search size="large" placeholder="input here" />
+                    </AntdAutoComplete>
                 </div>
-                <S.ButtonsWrapper>
-                    <S.ButtonWrapper>
-                        <Button type="primary">Primary Button</Button>
-                    </S.ButtonWrapper>
-                <Button type='primary'>Cancel</Button>
-                </S.ButtonsWrapper>
                 <S.DescWrapper>
                     Description
                 </S.DescWrapper>
